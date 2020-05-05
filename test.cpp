969ebca170693317556e5bd9bb56137acc66dc58
letranger <yen.yungchin@gmail.com>
2020-05-05 12:59:57 +0800

    2020/05/05

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a = 0;
+  printf("%d", a);
+  return 0;
+}
---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mv simpleSin.png DeepLearning
+mv colors.jpg DeepLearning
+mv line1.png DeepLearning
+mv line2.png DeepLearning
+mv simplePie.png DeepLearning
+mv simpleText.png DeepLearning
+mv simpleSubplot.png DeepLearning
+mv ./scores.csv DeepLearning
+mv scores.csv DeepLearning
+mv pandasPlot1.png DeepLearning
+mv pandasPlot2.png DeepLearning
+mv PandasPlot3.png DeepLearning
+mv PandasPlot4.png DeepLearning
+mv jsonLine.png DeepLearning
+mv jsonBar.png DeepLearning
