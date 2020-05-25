e9853e6864b3327aebfb5fb45ad55f6c057503ee
letranger <yen.yungchin@gmail.com>
2020-05-25 08:40:25 +0800

    2020 - 0525 剩進階練習

---
+++ myModule.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# name: myModule.py
+
+def pp(n):
+    return n+1
+
+def mm(n):
+    return n-1
--- test.py
+++ test.py
@@ -1,7 +1,37 @@
+from flask import Flask, jsonify
+import urllib.request as req
+import bs4
 
-x = 3
-print(x)
-y = 5
-print(y)
-z = x+y
-print(z)
+app = Flask(__name__)
+
+@app.route("/")
+
+def retrive():
+    url = "https://www.ptt.cc/bbs/Stock/index.html"
+    # 幫request加上一個header
+    request = req.Request(url, headers = {
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:72.0) Gecko/20100101 Firefox/72.0"
+    })
+    
+    with req.urlopen(request) as response:
+        data = response.read().decode("utf-8")
+    return data
+    root = bs4.BeautifulSoup(data,"html.parser")
+    titles = root.find("div", class_="title")
+    titles = root.find_all("div", class_="title")
+
+    res = []
+    for title in titles:
+        if title.a.string.find("Re:"):
+            res.append(title.a.string)
+    return res
+
+def hello():
+    title = "<title>Advanced Materials of Python</title>"
+    h1 = "<h1>Python based web</h1>"
+    p1 = "<p>Hello python</p>"
+    return jsonify(retrive())
+
+if __name__ == "__main__":
+    app.run()
+
